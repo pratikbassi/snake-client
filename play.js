306c0c2545d2918b30e2b7d5f21217e0ff1a0073
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-12 20:54:08 +0000

    created client.js

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
-const net = require('net');
+const { connect } = require('./client')
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.177',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
-  conn.on('data',(data) => {
-    console.log(data);
-  } )
-
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
