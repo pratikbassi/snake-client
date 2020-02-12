a8cd49d09a00e9ca151b7f3285d08f3bbe693607
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-12 20:59:44 +0000

    sending the server messages

--- client.js
+++ client.js
@@ -9,6 +9,13 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    
+    console.log('CONNECTED TO SERVER');
+  });
+  conn.write('Name:  PSB');
+
+
   conn.on('data',(data) => {
     console.log(data);
   } )
